3eba0b71a2649215a68f66335455afe74851c3b2
1shan <1shan@ratlab06.csie.ntust.edu.tw>
2016-01-27 18:24:50 +0800

    123

---
+++ Temple/Assets/CreatCP.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatCP : MonoBehaviour {
+	public GameObject CPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject CP_Clone = Instantiate (CPoint, Input.mousePosition, Quaternion.identity) as GameObject;
+	}
+
+}
---
+++ Temple/Assets/MouseEvent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseEvent : MonoBehaviour {
+	public GameObject CPpoint;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void MouseCreatCP(){
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray))
+				Instantiate (CPpoint, transform.position, transform.position);
+		}
+	}
+
+}
